75b8a942d50ce6e9eae1bd8555f75de736db9a11
Adore-dot <akinolaoreoluwa@gmail.com>
2026-02-06 03:28:29 +0100

    Implemented TaskManagement Controller with 1 property and two methods - GetAllTasks and GetTaskByID

--- TaskManagementAPI/Controllers/TasksController.cs
+++ TaskManagementAPI/Controllers/TasksController.cs
@@ -1,17 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using TaskManagementAPI.Models;
 
 namespace TaskManagementAPI.Controllers
 {
-    public class TasksController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
     {
-        private static List<TaskItem> MyTasks = new TaskItem
+        private static List<TaskItem> MyTasks = new List<TaskItem>
         {
-            Id = 1,
-            Title = "Dishes"
-            Description = "Do the dishes before 4pm",
-            IsCompleted = false,
-            CreatedDate = DateTime.Now
-        };       
+            new TaskItem { Id = 1, Title = "Dishes", Description = "Do the dishes before 4pm", IsCompleted = false, CreatedDate = DateTime.Now },
+            new TaskItem { Id = 2, Title = "Cleaning", Description = "Clean the house", IsCompleted = false, CreatedDate = DateTime.Now }
+        };
+
+        [HttpGet]
+        public IActionResult GetAllTasks()
+        {
+            return Ok(MyTasks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetTaskById(int id)
+        {
+            var task = MyTasks.FirstOrDefault(t => t.Id == id);
+            if(task != null)
+            {
+                return Ok(task);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
     }
 }
