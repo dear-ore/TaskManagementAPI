cfc8dd2a2564e5b4c1f9016a599a384bcdbc9d91
Adore-dot <akinolaoreoluwa@gmail.com>
2026-02-06 02:18:58 +0100

    Add project files.

---
+++ TaskManagementAPI/Controllers/TasksController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManagementAPI.Models;
+
+namespace TaskManagementAPI.Controllers
+{
+    public class TasksController : Controller
+    {
+        private static List<TaskItem> MyTasks = new TaskItem
+        {
+            Id = 1,
+            Title = "Dishes"
+            Description = "Do the dishes before 4pm",
+            IsCompleted = false,
+            CreatedDate = DateTime.Now
+        };       
+    }
+}
---
+++ TaskManagementAPI/Models/TaskItem.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskManagementAPI.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
