4e943ebf357513d04d71ad1008f5a826ee70e0ab
Adore-dot <akinolaoreoluwa@gmail.com>
2026-02-12 10:23:45 +0100

    Added validation to model

--- TaskManagementAPI/Models/TaskItem.cs
+++ TaskManagementAPI/Models/TaskItem.cs
@@ -1,10 +1,19 @@
-﻿namespace TaskManagementAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagementAPI.Models
+
 {
     public class TaskItem
     {
         public int Id { get; set; }
-        public required string Title { get; set; }
-        public required string Description { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string? Title { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public string? Description { get; set; }
         public bool IsCompleted { get; set; }
         public DateTime CreatedDate { get; set; }
     }
